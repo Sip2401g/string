2da25280a2d255cbc12e8891290102a9572fa001
Shubham Shelke <162583029+Sip2401g@users.noreply.github.com>
2026-04-23 23:23:23 +0530

    Update palindromeOrnot.cpp

--- palindromeOrnot.cpp
+++ palindromeOrnot.cpp
@@ -1,10 +1,10 @@
 #include<iostream>
 using namespace std;
 int main(){
-    string str = "kMadam";
+    string str = "Madam";
     bool isPalindrome = true;
     for(int i =0; i<str.size();i++){
-        if(tolower(str[i])!=tolower(str[str.size()-1-i])){
+        if(tolower(str[i])!=tolower(str[str.size()-1-i])){ // yaha -i dia taki har iteration pe ek piche aa sake simultaneously i++ toh ho he rah hai
             isPalindrome = false;
             break;
         }
@@ -16,3 +16,23 @@ int main(){
         cout<<"Not a Palindrome";
     }
 }
+
+//alternate mai 2 pointer lagate hau
+
+/* bool ispalindrome = true;
+int start =0, end = str.size()-1;
+while(start<end){
+if(str[start]!= str[end]){
+ispalindrome = false ;
+break;
+}
+start++;
+end--;
+}
+if(ispalindrome){
+cout<<"palindrome";}
+else{
+cout<<"not";
+}
+
+*/
